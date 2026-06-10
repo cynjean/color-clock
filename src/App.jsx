f7a1acd89738d458e8006d78595003ccb5355664
cynjean <cynthia.2056@yahoo.com>
2026-06-09 20:35:10 -0500

    Display the live date and time

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css';
 import { format } from 'date-fns';
-
+// Main application component that displays the clock
 function App() {
   return (
     <div>
+      {/*Displays the live date and time*/}
       <p>
+        {/* convert raw date into readable format using PPpp*/}
         {format(new Date(), 'PPpp')}
       </p>
     </div>
